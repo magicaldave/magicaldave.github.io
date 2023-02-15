3fe7fc767b82ed3acaba24465730126c73767c6c
magicaldave <smokydave@icloud.com>
2023-02-15 17:48:19 -0600

    Edit: added some debug outputs to the javascript.

--- AMAZE/test.js
+++ AMAZE/test.js
@@ -1,7 +1,12 @@
 function onClick() {
+    console.log("Requesting motion permissions");
     DeviceOrientationEvent.requestPermission().then(permissionState => {
 	if (permissionState === 'granted') {
+	    console.log("permission granted!");
 	    window.addEventListener('deviceorientation', () => {});
+	}
+	else {
+	    console.log("Perm function failed");
 	}
     });
 }
