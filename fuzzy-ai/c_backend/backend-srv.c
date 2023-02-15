236a00c2f4acfdb2f310afa4bd39380bdad2d791
magicaldave <smokydave@icloud.com>
2023-02-14 23:09:42 -0600

    Edit: Added functionality to grab endpoints and ensure only specific hosts and requests are granted traffic.

--- fuzzy-ai/c_backend/backend-srv.c
+++ fuzzy-ai/c_backend/backend-srv.c
@@ -1,9 +1,50 @@
 #include "sockets.h"
 #include <string.h>
-int grab_endpoint(char buffer[]);
+int extract_hostname(char *buffer)
+{
+	char *host_prefix = (strstr(buffer, "\r\nHost: ") + 8);
+	char *host = NULL, *auth = NULL, *accepted = "localhost";
+	int index;
+
+	for (index = 0; host_prefix[index] != 58; index++)
+		;
+
+	host_prefix[index] = '\0';
+        host = host_prefix;
+	/* This needs some safety features? */
+	auth = strcmp(host, accepted) == 0 ? "Authorized" : "Unauthorized";
+
+        printf("Grabbed hostname: %s : %s\n\n", host, auth);
+
+        return strcmp(host, accepted) == 0
+		? 1
+		: 0;
+}
+char *grab_endpoint(char buffer[])
+{
+	int index;
+	char endpoint[128];
+	char *endpoints[3] = {
+		"/generate-text",
+		"/generate-sound",
+		"/generate-img",
+	};
+
+	for (index = 0; buffer[index] && buffer[index] != 32; ++index);
+
+	bzero(endpoint, 128);
+	strncpy(endpoint, buffer, index);
+
+	printf("Endpoint requested: %s\n\n", endpoint);
+
+	for (index = 0; index <= 2; index++)
+		if (!strncmp(endpoint, endpoints[index], strlen(endpoints[index])))
+			return (endpoints[index]);
+        return (NULL);
+}
 int main(void)
 {
-	int sockn, newsock, client_sockfd, client_addrlen, reqcount = 0;
+	int sockn, newsock, client_sockfd, client_addrlen, reqcount = 0, authenticated = 0;
 	char buffer[BUFSIZE];
         char response[BUFSIZE] = "HTTP/1.1 200 OK\r\n"
 		"Server: SER-DAVE\r\n"
@@ -51,28 +92,19 @@ int main(void)
 		/* Sanitize buffers & read input data */
                 read_socket(client_sockfd, buffer);
                 WRITE_DATA;
+		/*
+		 * In order: checks for valid source of traffic,
+		 * valid request type, and valid endpoint
+		 */
+		authenticated = (extract_hostname(buffer) && !strncmp(buffer, "GET ", 4) && grab_endpoint(buffer + 4));
+		printf("Traffic originating from valid source: %s\n", authenticated ? "True" : "False");
 
-                if (strncmp(buffer, "GET ", 4))
-			write_socket(reqcount, client_sockfd, denial);
-		else
-		{
-                        write_socket(reqcount, client_sockfd, response);
-			/* An endpoint has now been captured, make sure it's a good one */
-			printf("\n\nSpace at: %d\n\n",
-			       grab_endpoint(buffer + 4));
-		}
+		write_socket(reqcount, client_sockfd, authenticated
+			     ? response
+			     : denial);
         }
 	printf("Webserver closing.\n");
 	return (0);
 }
 
-int grab_endpoint(char buffer[])
-{
-	int index;
-	char endpoint[128];
 
-	for (index = 0; buffer[index] && buffer[index] != 32; ++index);
-	strncpy(endpoint, buffer, index);
-	printf("Endpoint requested: %s\n\n", endpoint);
-        return (index);
-}
