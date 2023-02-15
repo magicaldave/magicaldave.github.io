ae7c8d9b78679074c15b2b52bdbf6156d7fcc629
magicaldave <smokydave@icloud.com>
2023-02-15 16:22:23 -0600

    Add: script to prompt for permission for iOS devicemotion

---
+++ AMAZE/test.js
@@ -0,0 +1,14 @@
+function onClick() {
+    // feature detect
+    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+	DeviceOrientationEvent.requestPermission()
+            .then(permissionState => {
+		if (permissionState === 'granted') {
+		    window.addEventListener('deviceorientation', () => {});
+		}
+            })
+            .catch(console.error);
+    } else {
+	// handle regular non iOS 13+ devices
+    }
+}
