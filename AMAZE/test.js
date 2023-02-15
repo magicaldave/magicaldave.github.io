2d7579369082866ecc140e575190227cfe4d0305
magicaldave <smokydave@icloud.com>
2023-02-15 17:29:35 -0600

    Edit: removed checking functionality of listener, no longer tries to determine if perms are required.

--- AMAZE/test.js
+++ AMAZE/test.js
@@ -1,14 +1,8 @@
 function onClick() {
-    // feature detect
-    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-	DeviceOrientationEvent.requestPermission()
-            .then(permissionState => {
-		if (permissionState === 'granted') {
-		    window.addEventListener('deviceorientation', () => {});
-		}
-            })
-            .catch(console.error);
-    } else {
-	console.log("Non-iOS 13 Device Detected!");
+    DeviceOrientationEvent.requestPermission().then(permissionState => {
+	if (permissionState === 'granted') {
+	    window.addEventListener('deviceorientation', () => {});
+	}
+    });
     }
 }
